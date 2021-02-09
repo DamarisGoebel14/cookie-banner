d66edb9e1aaf60b469b3a6605346a748d13def5f
Damaris <d4maris@devisland.de>
2021-02-09 14:55:52 +0100

    cookie consent with ga

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
+import {useEffect} from 'react'
 import './App.css';
+import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import Home from './Home'
+import Impressum from './Impressum'
+import * as ReactGA from "react-ga";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    Cookies.set("test", "nice");
+
+    const initGA = (id: string) => {
+        // if (process.env.NODE_ENV === "production") {
+            ReactGA.initialize(id);
+        // }
+    };
+
+    const handleAcceptCookie = () => {
+        if (process.env.REACT_APP_GOOGLE_ANALYTICS_ID) {
+            initGA(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+        }
+    };
+
+    const handleDeclineCookie = () => {
+        //remove google analytics cookies
+        Cookies.remove("_ga");
+        Cookies.remove("_gat");
+        Cookies.remove("_gid");
+    };
+
+    useEffect(() => {
+        const isConsent = getCookieConsentValue();
+        if (isConsent) {
+            handleAcceptCookie();
+        }
+    }, []);
+
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/impressum" component={Impressum}/>
+                </Switch>
+            </Router>
+
+            <CookieConsent
+                onAccept={handleAcceptCookie}
+                debug={true}
+                enableDeclineButton
+                declineButtonText="Decline (optional)"
+                onDecline={handleDeclineCookie}
+            >
+                This website uses cookies to enhance the user experience.{" "}
+                <span style={{ fontSize: "10px" }}>
+                    This bit of text is smaller :O
+                </span>
+            </CookieConsent>
+
+        </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
